105c14bb9d75b6e66e73d9f612031fc26901e1fe
Rudys NAtanael Acosta <natanael926@gmail.com>
2019-08-27 08:46:17 -0400

    Refactory app

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,13 +9,13 @@
                 <div class="col-lg-7">
                     <div class="p-5">
                         <div class="text-center">
-                            <h1 class="h4 text-gray-900 mb-4">{{ __('Register') }}</h1>
+                            <h1 class="h4 text-gray-900 mb-4">{{ __('Create an Account!') }}</h1>
                         </div>
                         <form class="user" method="POST" action="{{ route('register') }}">
                             @csrf
                             <div class="form-group row">
                                 <div class="col-sm-6 mb-3 mb-sm-0">
-                                    <input id="name" type="text" placeholder="{{ __('Name') }}" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                    <input id="name" type="text" placeholder="{{ __('Name') }}" class="form-control form-control form-control-user @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                     @error('name')
                                     <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
@@ -23,7 +23,7 @@
                                 </div>
 
                                 <div class="col-sm-6 mb-3 mb-sm-0">
-                                    <input id="email" type="email" placeholder="{{ __('E-Mail Address') }}" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                    <input id="email" type="email" placeholder="{{ __('E-Mail Address') }}" class="form-control form-control form-control-user @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                     @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -35,7 +35,7 @@
                             <div class="form-group row">
                                 <div class="col-sm-6 mb-3 mb-sm-0">
 
-                                        <input id="password" placeholder="{{ __('Password') }}" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                        <input id="password" placeholder="{{ __('Password') }}" type="password" class="form-control form-control form-control-user @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                         @error('password')
                                         <span class="invalid-feedback" role="alert">
@@ -44,7 +44,7 @@
                                         @enderror
                                 </div>
                                 <div class="col-sm-6">
-                                    <input id="password-confirm" placeholder="{{ __('Confirm Password') }}" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                    <input id="password-confirm" placeholder="{{ __('Confirm Password') }}" type="password" class="form-control form-control form-control-user" name="password_confirmation" required autocomplete="new-password">
                                 </div>
                             </div>
                             <button type="submit" class="btn btn-primary btn-user btn-block">

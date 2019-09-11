2c3598f13a1da63bf685afea5071c90197137ae9
Rudys NAtanael Acosta <natanael926@gmail.com>
2019-09-10 23:27:15 -0400

    Init Passport

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,6 +14,10 @@
                         </div>
                     @endif
 
+                        <passport-clients></passport-clients>
+                        <passport-authorized-clients></passport-authorized-clients>
+                        <passport-personal-access-tokens></passport-personal-access-tokens>
+
                     You are logged in!
                 </div>
             </div>

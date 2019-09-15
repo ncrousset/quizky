93a58e34ce96e197823b0783911e54817758a3bf
Rudys NAtanael Acosta <natanael926@gmail.com>
2019-09-14 20:29:34 -0400

    create the test quiz

---
+++ tests/Unit/QuizApiUnitTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class QuizApiUnitTest extends TestCase
+{
+
+    public function testCreate()
+    {
+
+    }
+
+    public function testIndex()
+    {
+
+    }
+
+    public function testShow()
+    {
+
+    }
+
+    public function testEdit()
+    {
+
+    }
+
+    public function testDelete()
+    {
+
+    }
+
+}

2bef2f93c76d0332cefcb246f3387b36c3fe42ed
Rudys NAtanael Acosta <natanael926@gmail.com>
2019-09-14 20:34:45 -0400

    Create de assert in testCreate

--- tests/Unit/QuizApiUnitTest.php
+++ tests/Unit/QuizApiUnitTest.php
@@ -9,7 +9,23 @@ class QuizApiUnitTest extends TestCase
 
     public function testCreate()
     {
+        $data = [
+            'user_id' => 1,
+            'title' => $this->faker->title,
+            'public' => rand(0,1)];
 
+        $response = $this->client
+            ->request('POST', env('APP_URL').'/api/quizzes', ['form_params' => $data]);
+
+        $this->assertEquals($response->getStatusCode(), 201);
+
+        $contentResponse = json_decode($response->getBody());
+
+        $this->assertIsObject($contentResponse);
+
+        $this->assertEquals($contentResponse->title, $data['title']);
+        $this->assertEquals($contentResponse->public, $data['public']);
+        $this->assertTrue($contentResponse->id > 0);
     }
 
     public function testIndex()

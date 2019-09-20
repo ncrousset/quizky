a620f454c07d96d3243efb7a5b639cdef317f0af
Rudys NAtanael Acosta <natanael926@gmail.com>
2019-09-19 23:03:51 -0400

    add Answers in router with Router::resource

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,8 @@ Route::post('register', 'Auth\RegisterController@register')->name('auth.register
 
 Route::middleware('auth:api')->group(function() {
     Route::namespace('Api')->group(function() {
-        Route::resource('quizzes', 'QuizzesController')->only([
-            'index', 'show', 'store', 'update'
-        ]);
-
-        Route::resource('questions', 'QuestionsController')->only([
-            'index', 'show', 'store', 'update'
-        ]);
+        Route::resource('quizzes', 'QuizzesController')->only(['index', 'show', 'store', 'update']);
+        Route::resource('questions', 'QuestionsController')->only(['index', 'show', 'store', 'update']);
+        Route::resource('answers', 'AnswersController')->only(['index', 'show', 'store', 'update']);
     });
 });

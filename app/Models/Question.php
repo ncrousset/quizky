0e60a28186ebf7bc323705986f3cef509d3b6478
Rudys NAtanael Acosta <natanael926@gmail.com>
2019-09-23 23:05:57 -0400

    Add relationships Answers

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -9,4 +9,9 @@ class Question extends Model
     protected $table = 'questions';
 
     protected $fillable = ['quiz_id', 'description', 'type', 'is_actived'];
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+    }
 }

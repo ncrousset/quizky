1e5dc3053d8c4973a83eb4d55ff84e199d7eb36e
Rudys NAtanael Acosta <natanael926@gmail.com>
2019-09-23 23:08:27 -0400

    Fix the router bug

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ Route::post('login', 'Auth\LoginController@login')->name('auth.login');
 
 Route::middleware('auth:api')->group(function() {
     Route::namespace('Api')->group(function() {
-        Route::get('answers/{question}', 'AnswersController@index')->name('answer.index');
+        Route::get('answers/{question}/list', 'AnswersController@index')->name('answer.index');
         Route::resource('quizzes', 'QuizzesController')->only(['index', 'show', 'store', 'update']);
         Route::resource('questions', 'QuestionsController')->only(['index', 'show', 'store', 'update']);
         Route::resource('answers', 'AnswersController')->only(['show', 'store', 'update']);
